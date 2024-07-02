a1d01717cbe6021f43970961f1de50a6a02a2d07
kimsojung <kimsojung1121@gmail.com>
2024-07-03 00:47:37 +0900

    cat-care-back initial commit

---
+++ src/main/java/com/nerd2/catcare/CatcareApplication.java
@@ -0,0 +1,13 @@
+package com.nerd2.catcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatcareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nerd2/catcare/CatcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nerd2.catcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
